6a7d561eefea1a0027f73bc924857dbb7b72ba61
krishnachourasia <kchourasia66@gmail.com>
2020-04-12 05:37:16 +0530

    Profile Added.

--- kwitter/urls.py
+++ kwitter/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/', user_views.profile, name='profile'),
     path("", include('blog.urls') ),
 ]
