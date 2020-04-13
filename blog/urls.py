c83233a93fcc2c61a920d415816dba2a86aef778
krishnachourasia <kchourasia66@gmail.com>
2020-04-14 05:22:18 +0530

    Post CRUD.Password Reset Via mail.(email BACKEND variables set)

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,20 @@
 from .views import (home,
-                    about)
+                    PostListView,
+                    PostDetailView,
+                    PostCreateView,
+                    PostUpdateView,
+                    PostDeleteView,
+                    UserPostListView,
+                    about,)
 from django.urls import path
 
 
 urlpatterns = [
-    path('', home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path("post/<int:pk>", PostDetailView.as_view(), name='blog-post'),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name='post-update'),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name='post-delete'),
     path("about/", about, name='blog-about')
 ]
